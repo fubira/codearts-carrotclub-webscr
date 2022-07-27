33ea5349701a24c08b5d8094bc9980bbdac3cda3
fubira <fubira@gmail.com>
2022-07-27 20:44:30 +0900

    parser

--- src/convert.ts
+++ src/convert.ts
@@ -1,12 +1,13 @@
 import 'dotenv/config'
 import log4js from 'log4js';
+import { readFileSync } from 'fs';
 import { exit } from 'process';
+import FastGlob from 'fast-glob';
+import parse from 'node-html-parser';
 import commandLineArgs from 'command-line-args';
 import commandLineUsage from 'command-line-usage';
 
-import { scraping, ScrapingInfoType, ScrapingDataType } from './keibabook';
-import { mkdirSync, writeFileSync, existsSync, readFileSync } from 'fs';
-import FastGlob from 'fast-glob';
+import { RaceData, RaceInfo, DataCourse, DataEntry, CourseType, CourseDirection, CourseCondition, CourseWeather, HorseSex } from './types';
 
 const logger = log4js.getLogger();
 
@@ -40,13 +41,109 @@ log4js.configure({
   }
 });
 
-async function parseFile(file: string) {
+async function parseCourse(info: RaceInfo, entriesHtml: string): Promise<DataCourse> {
+  const root = parse(entriesHtml);
 
+  /// レースコース情報取得
+  const raceParams = root.querySelectorAll('div.racetitle_sub > p');
+  const courseOpt = raceParams[0].textContent.trim(); 
+  const courseInfo = raceParams[1].textContent.trim(); 
+
+  const [,
+    distance,
+    type,
+    direction,
+    weather,
+    condition
+  ] = courseInfo.match(/(\d+)m\s\((.+)・(.+)\)\s(.+)・(.+?)\s/);
+
+  return {
+    id: Number(info.courseId),
+    name: info.courseName,
+    distance: Number(distance),
+    type: type as CourseType,
+    direction: direction as CourseDirection,
+    weather: weather as CourseWeather,
+    condition: condition as CourseCondition,
+    option: courseOpt,
+  }
+}
+
+async function parseEntries(entriesHtml: string): Promise<DataEntry[]> {
+  const root = parse(entriesHtml);
+
+  /// レース情報取得
+  const raceParams = root.querySelectorAll('div.racetitle_sub > p');
+  const raceOpt = raceParams[0].textContent.trim(); 
+  const raceCourseInfo = raceParams[1].textContent.trim(); 
+
+  const [,
+    courseDistance,
+    courseType,
+    courseDirection,
+    courseAir,
+    courseStat
+  ] = raceCourseInfo.match(/(\d+)m\s\((.+)・(.+)\)\s(.+)・(.+?)\s/);
+
+  console.log(raceOpt, courseDistance, courseType, courseDirection, courseAir, courseStat);
+
+  /// 馬情報取得
+  const tbody = root.querySelector('table.syutuba_sp tbody');
+
+  if (!tbody) {
+    return;
+  }
+
+  const tr = tbody.querySelectorAll('tr');
+
+  const result = tr.map((record): DataEntry => {
+    const bracketId = record.querySelector("td.waku > p").textContent.trim();
+    const horseId = record.querySelector("td.umaban").textContent.trim();
+    const horseName = record.querySelectorAll("td.left p")[0].textContent.trim();
+    const horseSubInfo = record.querySelectorAll("td.left p")[1].textContent.trim();
+    const weightInfo = record.querySelectorAll("td.lh1 p")[0].textContent.trim();
+    const odds = record.querySelectorAll("td.lh1 p")[1].textContent.trim();
+    const oddsRankInfo = record.querySelectorAll("td.lh1 p")[2].textContent.trim();
+
+    const [, horseSex, horseAge, jockyName, handicap] = horseSubInfo.match(/(\S+?)(\d+)\s+(\S+?)\s+(\S+)/);
+    const [horseWeight, horseWeightDiff] = weightInfo.replace(/[()]/g, ' ').split(' ');
+    const oddsRank = oddsRankInfo.replace('人気', '');
+
+    return {
+      bracketId: Number(bracketId),
+      horseId: Number(horseId),
+      horseName: horseName,
+      horseSex: horseSex as HorseSex,
+      horseAge: Number(horseAge),
+      jockyName: jockyName,
+      handicap: Number(handicap),
+      horseWeight: Number(horseWeight),
+      horseWeightDiff: horseWeightDiff && Number(horseWeightDiff),
+      odds: Number(odds),
+      oddsRank: Number(oddsRank),
+    }
+  });
+
+  return result;
+}
+
+async function parseFile(file: string) {
+  const dataJson = readFileSync(file);
+  const { data, info } = JSON.parse(dataJson.toString()) as RaceData;
+
+  logger.info('-------------');
+  logger.info(`${info.date} ${info.courseName}(${info.courseId}) ${info.raceNo}R ${info.raceTitle}`);
+
+  const course = await parseCourse(info, data.entries);
+  const entries = await parseEntries(data.entries);
+
+  console.log(JSON.stringify(course));
+  console.log(JSON.stringify(entries));
 }
 
 
 FastGlob(`${args["root"]}/**/*.json`, { onlyFiles: true }).then((files) => {
-  const sortedFiles = files.slice(0, 5);
+  const sortedFiles = files.slice(0, 20);
 
   Promise.all(sortedFiles.map(async (file) => {
     await parseFile(file);
--- src/types.ts
+++ src/types.ts
@@ -25,3 +25,87 @@ export interface RaceRawData {
   blood: string;
   result: string;
 }
+
+export type CourseType = '芝' | 'ダート';
+export type CourseDirection = '左' | '右';
+export type CourseWeather = '晴' | '曇' | '雨' | '小雨' | '雪' | '小雪';
+export type CourseCondition = '良' | '稍重' | '重' | '不良';
+
+export interface DataRace {
+  course: DataCourse;
+
+  entries: DataEntry[];
+}
+
+export interface DataCourse {
+  // コースID
+  id: number;
+
+  // 競馬場名
+  name: string;
+
+  // 距離
+  distance: number;
+  
+  // ダート/芝
+  type: CourseType;
+
+  // 右周り左回り
+  direction: CourseDirection;
+
+  // 天候
+  weather: CourseWeather;
+
+  // 馬場状態
+  condition: CourseCondition;
+
+  // 付随情報
+  option: string;
+}
+
+export type HorseSex = '牡' | '牝' | 'セン';
+
+export interface DataEntry {
+  // 枠版
+  bracketId: number;
+
+  // 馬番
+  horseId: number;
+
+  // 馬名
+  horseName: string;
+
+  // 性別
+  horseSex: HorseSex;
+
+  // 年齢
+  horseAge: number;
+  
+  // ジョッキー
+  jockyName: string;
+
+  // ハンデ
+  handicap: number;
+
+  // 馬体重
+  horseWeight: number;
+
+  // 馬体重増減
+  horseWeightDiff: number;
+
+  // 人気
+  odds: number;
+
+  // 人気順
+  oddsRank: number;
+
+  // 血統
+  horseBlood?: string;
+
+  // 調教
+  trainingLogs?: string;
+}
+
+export interface DataResult {
+  _: null;
+}
